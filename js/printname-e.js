d410d1143a4c274dbd8772da1fa08ea7fe655824
t-m-comp <takuya.maekawa@gmail.com>
2024-08-15 11:19:47 +0900

    publication update

--- js/printname-e.js
+++ js/printname-e.js
@@ -1,5 +1,5 @@
 <!--
-document.write("<font size=\"+1\"><b>Takuya Maekawa</b></font><br><br>Osaka University<br>Associate Professor<br>Ph. D. (Information Science)<br>",
+document.write("<font size=\"+1\"><b>Takuya Maekawa</b></font><br><br>Osaka University<br>Associate Professor<br>Ph. D. (Information Science and Technology)<br>",
 "maekawa<img src=\"./mail/atm.png\"/>ist.osaka-u.ac.jp<br>",
 "takuya.maekawa<img src=\"./mail/atm.png\"/>acm.org");
 // -->
