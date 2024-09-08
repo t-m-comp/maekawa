2d1ec484453e324bdb34e46b690b83f7aa18a41d
t-m-comp <takuya.maekawa@gmail.com>
2024-09-08 21:26:05 +0900

    prof

--- js/printname-j.js
+++ js/printname-j.js
@@ -1,5 +1,5 @@
 <!--
-document.write("<font size=\"+1\"><b>前川卓也 </b></font><br><br>大阪大学大学院 情報科学研究科<br>准教授<br>博士(情報科学) <br>",
+document.write("<font size=\"+1\"><b>前川卓也 </b></font><br><br>大阪大学 高等共創研究院/大学院情報科学研究科<br>教授<br>博士(情報科学) <br>",
 "maekawa<img src=\"./mail/atm.png\"/>ist.osaka-u.ac.jp<br>",
 "takuya.maekawa<img src=\"./mail/atm.png\"/>acm.org"
 );
