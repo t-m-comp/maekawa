d1c720fbf40becd07441de1afe3837e3b8f41e19
t-m-comp <takuya.maekawa@gmail.com>
2024-11-29 00:18:26 +0900

    bio update

--- js/printname-e.js
+++ js/printname-e.js
@@ -1,5 +1,5 @@
 <!--
-document.write("<font size=\"+1\"><b>Takuya Maekawa</b></font><br><br>Osaka University, Institute for Advanced Co-Creation Studies<br>Professor / Distinguished Professor<br>Ph. D. (Information Science and Technology)<br>",
+document.write("<font size=\"+1\"><b>Takuya Maekawa</b></font><br><br>Institute for Advanced Co-Creation Studies, Osaka University<br>Professor / Distinguished Professor<br>Ph. D. (Information Science and Technology)<br>",
 "maekawa<img src=\"./mail/atm.png\"/>ist.osaka-u.ac.jp<br>",
 "takuya.maekawa<img src=\"./mail/atm.png\"/>acm.org");
 // -->
